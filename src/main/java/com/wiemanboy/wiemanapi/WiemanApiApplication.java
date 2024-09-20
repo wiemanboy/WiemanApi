9e0cb4cc649a5a1ec932605cb7b9315505a0b46a
jarno <wiemanboy@gmail.com>
2024-09-20 21:35:08 +0200

    Initial commit

---
+++ src/main/java/com/wiemanboy/wiemanapi/WiemanApiApplication.java
@@ -0,0 +1,13 @@
+package com.wiemanboy.wiemanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiemanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiemanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiemanboy/wiemanapi/WiemanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiemanboy.wiemanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiemanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
