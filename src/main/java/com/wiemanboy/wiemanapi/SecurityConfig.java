af0fb2013c115bbda3831896325149f1ff7b317c
jarno <wiemanboy@gmail.com>
2024-10-09 19:52:00 +0200

    ADD: auth0 SSO configuration

---
+++ src/main/java/com/wiemanboy/wiemanapi/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.wiemanboy.wiemanapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/api/profiles/actuator/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/profiles/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/profiles/{name}/{locale}").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .defaultSuccessUrl("/api/profiles/docs/")
+                        .failureUrl("/")
+                );
+        return http.build();
+    }
+}
