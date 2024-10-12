be1fb6e51ec7af457af4fe95bc8bc10d87f605cf
jarno <wiemanboy@gmail.com>
2024-10-12 23:34:10 +0200

    FIX: tests should now also work in ci/cd pipeline

--- src/test/java/com/wiemanboy/wiemanapi/config/TestSecurityConfig.java
+++ src/test/java/com/wiemanboy/wiemanapi/config/TestSecurityConfig.java
@@ -2,8 +2,10 @@ package com.wiemanboy.wiemanapi.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static org.mockito.Mockito.mock;
 
@@ -13,6 +15,11 @@ public class TestSecurityConfig {
     @Bean
     public JwtDecoder jwtDecoder() {
         return mock(JwtDecoder.class); // Mock JwtDecoder
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return mock(SecurityFilterChain.class);
     }
 
     @Bean
