0df17a7398aacbd917952580cedf3d036103c9b1
jarno <wiemanboy@gmail.com>
2024-10-19 15:17:52 +0200

    FIX: remove id from create dto

--- src/main/java/com/wiemanboy/wiemanapi/presentation/dto/request/CreateProfileDto.java
+++ src/main/java/com/wiemanboy/wiemanapi/presentation/dto/request/CreateProfileDto.java
@@ -7,7 +7,6 @@ import com.wiemanboy.wiemanapi.presentation.dto.response.SocialDto;
 import java.util.List;
 
 public record CreateProfileDto(
-        String id,
         String firstName,
         String lastName,
         String username,
